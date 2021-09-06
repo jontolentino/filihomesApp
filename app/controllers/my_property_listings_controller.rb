8921669b388ab8ef9fc4768edd427a70bb27f2e3
Jon Tolentino <jontolentino123@gmail.com>
2021-09-06 21:03:55 +0800

    file created newlisting

--- app/controllers/my_property_listings_controller.rb
+++ app/controllers/my_property_listings_controller.rb
@@ -1,11 +1,15 @@
 class MyPropertyListingsController < ApplicationController
     
     def my_listing
-        @my_listings = MyPropertyListing.all.reverse_order
+        @my_listings = MyPropertyListing.all.reverse_order()
     end
 
     def show_listing
         
+    end
+
+    def new_listing
+        @my_listing = MyPropertyListing.new()
     end
 
     def create_listing
