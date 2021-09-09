fb4c2fbc52bca53fa785d118b2bdfff38dae565b
Jon Tolentino <jontolentino123@gmail.com>
2021-09-09 16:01:05 +0800

    more my listing views update and authorizations

--- app/controllers/my_property_listings_controller.rb
+++ app/controllers/my_property_listings_controller.rb
@@ -1,5 +1,6 @@
 class MyPropertyListingsController < ApplicationController
-    
+    before_action :check_if_valid_user, only: [:new_listing, :create_listing, :delete_listing, :edit_listing, :patch_listing]
+
     def my_listing
         @my_listings = MyPropertyListing.where(user_id: params[:id]).reverse_order
         @user = User.find_by(id: params[:id])
@@ -10,22 +11,37 @@ class MyPropertyListingsController < ApplicationController
     end
 
     def new_listing
-        @my_listing = MyPropertyListing.new()
+        # @my_listing = MyPropertyListing.new()
+
+        if params[:id].to_s == session[:user_id].to_s
+            @my_listing = MyPropertyListing.new()
+        else
+            flash[:notice] = "You cannot create a listing here"
+            redirect_to my_listing_path()
+        end
+
     end
 
     def create_listing
-        @listing = Current.user.my_property_listings.build(new_listing_params)
 
-        if @listing.save
-            flash[:notice] = "Success: Created a new listing"
-            redirect_to my_listing_path(id: session[:user_id])
-        else
-            flash[:notice] = "Failed"
-            redirect_to new_listing_path(id: session[:user_id])
+        if Current.user.id
+            @listing = Current.user.my_property_listings.build(new_listing_params)
+
+            if @listing.save
+                flash[:notice] = "Success: Created a new listing"
+                redirect_to my_listing_path(id: session[:user_id])
+            else
+                flash[:notice] = "Failed"
+                redirect_to new_listing_path(id: session[:user_id])
+            end
         end
+
+       
     end
 
     def delete_listing
+
+
         @property = MyPropertyListing.find_by(user_id: params[:id], id: params[:prop_id])
 
         if @property.destroy
@@ -38,7 +54,18 @@ class MyPropertyListingsController < ApplicationController
     end
 
     def edit_listing
+        # if Current.user.id == params[:id]
+        #     @my_listing = MyPropertyListing.find_by(user_id: params[:id], id: params[:prop_id])
+        # else
+        #     flash[:notice] = "You cannot edit this listing"
+        #     redirect_to my_listing_path(id: session[:user_id])
+        # end
         @my_listing = MyPropertyListing.find_by(user_id: params[:id], id: params[:prop_id])
+
+        if Current.user.id != @my_listing.user_id
+            flash[:notice] = "You cannot edit this listing"
+            redirect_to show_listing_path()
+        end
     end
 
     def patch_listing
@@ -58,6 +85,13 @@ class MyPropertyListingsController < ApplicationController
 
     def new_listing_params
         params.require(:my_listing).permit(:title, :location_name, :lot_area, :floor_area, :bedroom_count, :bathroom_count, :details, :property_image)
+    end
+
+    def check_if_valid_user
+        if Current.user == nil
+            flash[:notice] = "You don't have access."
+            redirect_to sessions_new_login_path()
+        end
     end
 
 end
